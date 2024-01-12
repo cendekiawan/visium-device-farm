40adbb3bf3146946a1f849e7c2aa95042959b3b8
cendekiawan <onyimomo06@gmail.com>
2024-01-12 14:17:59 +0700

    update test suite collection

--- Test Suites/Device Farm - UAT.ts
+++ Test Suites/Device Farm - UAT.ts
@@ -24,7 +24,7 @@
             </runConfigurationData>
             <runConfigurationId>Remote</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Feature Register - Android 1</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
@@ -44,7 +44,7 @@
             </runConfigurationData>
             <runConfigurationId>Remote</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Feature Register - Android 2</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
