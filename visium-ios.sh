839f2754d91045287669bd97c541202d9a556715
cendekiawan <35634375+cendekiawan@users.noreply.github.com>
2024-01-15 11:20:54 +0700

    Update visium-ios.sh

--- visium-ios.sh
+++ visium-ios.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
 set -x
 
-# step 1: check and install jq
-# Check if jq is installed
-if ! command -v jq &> /dev/null; then
-    echo "jq is not installed. Installing..."
-    
-    # Update package lists and install jq
-    apt-get update
-    apt-get install -y jq
 
-    # Check installation success
-    if command -v jq &> /dev/null; then
-        echo "jq has been successfully installed."
-    else
-        echo "Failed to install jq. Please install it manually."
-    fi
-else
-    echo "jq is already installed."
-fi
-
-
-# step 2: get all devide id of iOS
+# step 1: get all devide id of iOS
 # Make the GET request and capture the JSON response
 response=$(curl -s -X GET https://farmdemo.visiumlabs.com/api/devices?os=iOS \
 -H "Content-Type: application/json" \
 -H "X-VisiumFarm-Api-Key: 2q3Ue23QgD.Xj0TMbjEpzr2Zf6PfngagnetePGiOjubCr6aUwN0")
 
 # Extract the 'id' attributes from the JSON response and format output with double quotes
-ids=$(echo "$response" | jq -r '[.[] | .deviceId] | map("\"" + . + "\"") | join(",")')
+ids='"52faf304d5147795cac1003572fb3b50d74ef5d4"'
 
 
 # Output the extracted 'id' attributes in the required format
@@ -37,7 +18,7 @@ echo "$ids"
 export ids=$ids
 
 
-# step 3: upload IPA file and get the id of IPA file
+# step 2: upload IPA file and get the id of IPA file
 # Set the necessary variables
 API_KEY="2q3Ue23QgD.Xj0TMbjEpzr2Zf6PfngagnetePGiOjubCr6aUwN0"
 FILE_PATH="testingVisium.ipa"
@@ -58,7 +39,7 @@ echo "The extracted appId is: $appId"
 
 export appId=$appId
 
-# step 4: Install the IPA in all iOS devices
+# step 3: Install the IPA in all iOS devices
 curl -X POST https://farmdemo.visiumlabs.com/api/apk/install \
 -H "Content-Type: application/json" \
 -H "X-VisiumFarm-Api-Key: 2q3Ue23QgD.Xj0TMbjEpzr2Zf6PfngagnetePGiOjubCr6aUwN0" \
