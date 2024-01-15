9fc139782ca1384c022bfcdf30f4cbbec149fb37
cendekiawan <35634375+cendekiawan@users.noreply.github.com>
2024-01-15 08:45:13 +0700

    Update visium-android.sh

--- visium-android.sh
+++ visium-android.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 set -x
 
-# step 1: check and install jq
-# Check if jq is installed
-if ! command -v jq &> /dev/null; then
-    echo "jq is not installed. Installing..."
-    
-    # Update package lists and install jq
-    apt-get update
-    apt-get install -y jq
-
-    # Check installation success
-    if command -v jq &> /dev/null; then
-        echo "jq has been successfully installed."
-    else
-        echo "Failed to install jq. Please install it manually."
-    fi
-else
-    echo "jq is already installed."
-fi
-
 
 # step 2: get all devide id of Android
 # Make the GET request and capture the JSON response
@@ -28,7 +9,7 @@ response=$(curl -s -X GET https://farmdemo.visiumlabs.com/api/devices?os=Android
 -H "X-VisiumFarm-Api-Key: 2q3Ue23QgD.Xj0TMbjEpzr2Zf6PfngagnetePGiOjubCr6aUwN0")
 
 # Extract the 'id' attributes from the JSON response and format output with double quotes
-ids=$(echo "$response" | jq -r '[.[] | .deviceId] | map("\"" + . + "\"") | join(",")')
+ids=sh(script: $(echo "$response" | jq -r '[.[] | .deviceId] | map("\"" + . + "\"") | join(",")')
 
 
 # Output the extracted 'id' attributes in the required format
